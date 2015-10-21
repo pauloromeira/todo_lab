49e162fa95e638ec888bd2e1f82ceb432a4452f7
Paulo Romeira <paulo@pauloromeira.com>
2015-10-21 18:32:28 -0300

    Add client-side components

--- todo_web/static/js/main.js
+++ todo_web/static/js/main.js
@@ -1,8 +1,11 @@
 var app = angular.module('todo',[
     'ui.router',
+    'ngDraggable',
 ]);
 
-app.constant('BASE_URL', '');
+app.constant('BASE_URL', 'http://localhost:8000/api/');
+app.constant('LISTS_PATH', 'todo_lists/');
+app.constant('ITEMS_PATH', 'todo_items/');
 
 app.config(function($stateProvider, $urlRouterProvider){
   $stateProvider
@@ -13,12 +16,56 @@ app.config(function($stateProvider, $urlRouterProvider){
     });
 });
 
-app.controller('MainController', 
-    function ($scope, LoginService, TodoService, $state) {
+app.controller('MainController', function ($scope, todoFactory, $state) {
+  $scope.filterDone = function (items){
+    return items.filter(function(i){ return i.done });
+  };
+  todoFactory.list.all().then(function(res){
+    $scope.todo_lists = res.data;
+  });
+
 });
 
-app.service('LoginService', function ($http, BASE_URL) {
-});
+app.factory('todoFactory', function ($http, BASE_URL, LISTS_PATH, ITEMS_PATH) {
+  var factory = {};
 
-app.service('TodoService', function ($http, BASE_URL) {
+  var all = function(resourcePath){
+    return function() { 
+      return $http.get(BASE_URL + resourcePath) 
+    };
+  };
+
+  var create = function(resourcePath){
+    return function(element) { 
+      return $http.post(BASE_URL + resourcePath, element) 
+    };
+  };
+
+  var update = function(resourcePath){
+    return function(element) { 
+      return $http.put(BASE_URL + resourcePath + element.id, element) 
+    };
+  };
+
+  var remove = function(resourcePath){
+    return function(id) { 
+      return $http.delete(BASE_URL + resourcePath + id) 
+    };
+  };
+
+  factory.list = {
+    all : all(LISTS_PATH),
+    create : create(LISTS_PATH),
+    update : update(LISTS_PATH),
+    remove : remove(LISTS_PATH)
+  };
+
+  factory.item = {
+    all : all(ITEMS_PATH),
+    create : create(ITEMS_PATH),
+    update : update(ITEMS_PATH),
+    remove : remove(ITEMS_PATH)
+  };
+
+  return factory;
 });
