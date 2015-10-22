aff0a75595045e88569562aa3a2acbdcc3e7930b
Paulo Romeira <paulo@pauloromeira.com>
2015-10-22 02:52:12 -0300

    Create todo list/item add buttons and edit mode

--- todo_web/static/js/main.js
+++ todo_web/static/js/main.js
@@ -17,13 +17,30 @@ app.config(function($stateProvider, $urlRouterProvider){
 });
 
 app.controller('MainController', function ($scope, todoFactory, $state) {
+  $scope.todo_lists = [];
+  $scope.edit_mode = false;
+
   $scope.filterDone = function (items){
     return items.filter(function(i){ return i.done });
   };
+
+  $scope.newTodoList = function () {
+    var todo_list = {};
+    todoFactory.list.add(todo_list).then(function(res){
+      $scope.todo_lists.push(res.data);
+    });
+  };
+
+  $scope.newTodoItem = function (todo_list) {
+    var todo_item = { todo_list: todo_list.id };
+    todoFactory.item.add(todo_item).then(function(res){
+      todo_list.items.push(res.data);
+    });
+  };
+
   todoFactory.list.all().then(function(res){
     $scope.todo_lists = res.data;
   });
-
 });
 
 app.factory('todoFactory', function ($http, BASE_URL, LISTS_PATH, ITEMS_PATH) {
@@ -35,7 +52,7 @@ app.factory('todoFactory', function ($http, BASE_URL, LISTS_PATH, ITEMS_PATH) {
     };
   };
 
-  var create = function(resourcePath){
+  var add = function(resourcePath){
     return function(element) { 
       return $http.post(BASE_URL + resourcePath, element) 
     };
@@ -55,14 +72,14 @@ app.factory('todoFactory', function ($http, BASE_URL, LISTS_PATH, ITEMS_PATH) {
 
   factory.list = {
     all : all(LISTS_PATH),
-    create : create(LISTS_PATH),
+    add : add(LISTS_PATH),
     update : update(LISTS_PATH),
     remove : remove(LISTS_PATH)
   };
 
   factory.item = {
     all : all(ITEMS_PATH),
-    create : create(ITEMS_PATH),
+    add : add(ITEMS_PATH),
     update : update(ITEMS_PATH),
     remove : remove(ITEMS_PATH)
   };
